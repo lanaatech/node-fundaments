036521afce0dbc3cf08d11e2d97d83e5b5e03bcd
lanacode <artedasredes@gmail.com>
2026-01-13 15:29:33 -0300

    atualizacao de atividades

--- modulo_01/atv09.js
+++ modulo_01/atv09.js
@@ -1,1 +1,7 @@
 const readlineSync = require("readline-sync");
+
+const nome = readlineSync.question("Digite o seu nome: ");
+const idade = readlineSync.questionInt("Digite a sua idade: ");
+const cidade = readlineSync.question("Digite a cidade em que voce nasceu: ");
+
+console.log(`Olá ${nome}, voce tem ${idade} anos de idade e nasceu em ${cidade}`);
